aad2f832ecedf864fe44147dd8b56c209ba05649
janisuhanda <janisuhanda@outlook.co.id>
2025-07-25 21:57:54 +0700

    JWT CRUD Product

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user',function(Request $request){
     return $request->user();
 });
 
-Route::apiResource('/products',ProductController::class);
+Route::apiResource('/products',ProductController::class)->middleware('auth:api');
